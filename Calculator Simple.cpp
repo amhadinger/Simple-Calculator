3c1b25b26d58f11a76eebe7161fef5cd609c6c6d
amhadinger <amhadinger@users.noreply.github.com>
2017-01-02 19:25:04 -0500

    Add files via upload

---
+++ Calculator Simple.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using int32 = int;
+void Intro();
+void Intro() {
+	std::cout << "Welcome to Simple Calulator" << std::endl;
+	std::cout << "Made by Austin Hadinger for Education Fair Project" << std::endl;
+	void TakeInput();
+}
+
+void TakeInput() {
+	int Answer = 0;
+	std::cout << "Type a number";
+	int32 firstNumber;
+	std::cin >> firstNumber;
+	std::cout << std::endl;
+	std::cout << "Do you want to add, subtract, multiply, or divide?" << std::endl;
+	std::string symbol = "";
+	getline(std::cin, symbol);
+	std::cout << std::endl;
+	std::cout << "Type the second number" << std::endl;
+	int32 secondNumber;
+	std::cin >> secondNumber;
+	
+	if (symbol == "-") {
+		Answer = firstNumber - secondNumber;
+		std::cout << "Your Answer is:" << Answer << std::endl;
+	}
+	if (symbol == "+") {
+		Answer = firstNumber + secondNumber;
+		std::cout << "Your Answer is:" << Answer << std::endl;
+	}
+	if (symbol == "/") {
+		Answer = firstNumber / secondNumber;
+		std::cout << "Your Answer is:" << Answer << std::endl;
+	}
+	if (symbol == "*") {
+		Answer = firstNumber * secondNumber;
+		std::cout << "Your Answer is:" << Answer;
+	}
+}
+
