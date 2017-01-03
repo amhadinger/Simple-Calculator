8a0170f4255162865960c7875cc4db4b035b2589
amhadinger <amhadinger@users.noreply.github.com>
2017-01-03 12:44:11 -0500

    Update Calculator Simple.cpp

--- Calculator Simple.cpp
+++ Calculator Simple.cpp
@@ -1,43 +1,47 @@
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 
-using int32 = int;
-void Intro();
-void Intro() {
+void main() {
 	std::cout << "Welcome to Simple Calulator" << std::endl;
-	void TakeInput();
-}
-
-void TakeInput() {
 	int Answer = 0;
 	std::cout << "Type a number";
-	int32 firstNumber;
+	int firstNumber = 0;
+	std::cout << std::endl;
 	std::cin >> firstNumber;
 	std::cout << std::endl;
-	std::cout << "Do you want to add, subtract, multiply, or divide?" << std::endl;
-	std::string symbol = "";
-	getline(std::cin, symbol);
+	std::cout << "Do you want to add, subtract, multiply, or divide?";
 	std::cout << std::endl;
+	std::string symbol = "";
+	std::cin >> symbol;
+	std::cout << std::endl;
+	if (symbol != "-" && symbol != "+" && symbol != "/" && symbol != "*") {
+		std::cout << "!!INCORRECT SYMBOL PLEASE TRY AGAIN!!";
+		std::cout << std::endl;
+		std::cin >> symbol;
+	}
 	std::cout << "Type the second number" << std::endl;
-	int32 secondNumber;
+	int secondNumber = 0;
 	std::cin >> secondNumber;
-	
+
 	if (symbol == "-") {
 		Answer = firstNumber - secondNumber;
-		std::cout << "Your Answer is:" << Answer << std::endl;
+		std::cout << "Your Answer is:" << Answer;
+		std::cout << std::endl;
 	}
 	if (symbol == "+") {
 		Answer = firstNumber + secondNumber;
-		std::cout << "Your Answer is:" << Answer << std::endl;
+		std::cout << "Your Answer is:" << Answer;
+		std::cout << std::endl;
 	}
 	if (symbol == "/") {
 		Answer = firstNumber / secondNumber;
-		std::cout << "Your Answer is:" << Answer << std::endl;
+		std::cout << "Your Answer is:" << Answer;
+		std::cout << std::endl;
 	}
 	if (symbol == "*") {
 		Answer = firstNumber * secondNumber;
 		std::cout << "Your Answer is:" << Answer;
+		std::cout << std::endl;
 	}
 }
 
